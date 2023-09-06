468db4131632f82f0c5fbf507d8d6bde29dd46c6
Gautam Sodhi <129111379+Iam-Sodhi@users.noreply.github.com>
2023-09-07 04:48:57 +0530

    m

--- next.config.js
+++ next.config.js
@@ -3,11 +3,13 @@ const nextConfig = {
   experimental: { // this is to use server actions in the foem
     serverActions: true,
   },
-  webpack: (config) => {
-    config.externals.push({
-      "utf-8-validate": "commonjs utf-8-validate",
-      bufferutil: "commonjs bufferutil"
-    });
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
 
     return config;
   },
