b3b7597d698676573e4c96abbac2f79fe493bed8
Gautam Sodhi <129111379+Iam-Sodhi@users.noreply.github.com>
2023-09-24 05:43:42 +0530

    provider added

--- app/channel/layout.tsx
+++ app/channel/layout.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/context/theme-provider";
 import ModalProvider from "@/context/ModalProvider";
+import { SocketProvider } from "@/context/socketProvider";
 
 
 export default function RootLayout({
@@ -15,9 +16,12 @@ export default function RootLayout({
             defaultTheme="dark"
             enableSystem={false}
             storageKey="discord-theme"
-          >
+          >  
+          <SocketProvider >
+
               <ModalProvider />
                 {children}
+          </SocketProvider>
           </ThemeProvider>
       </div>
   );
